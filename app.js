e7f604516b18433fd7ef39831111f9b952f7bf96
LGHABD <laghzabdou4@gmail.com>
2026-06-03 11:19:18 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").innerText =
+        "Bonjour ! Vous avez cliqué sur le bouton.";
+});
